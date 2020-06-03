37e391b81c76b0f6a0f3b27fba320bc229318104
Alice Zhou <alicezhou0805@gmail.com>
2020-06-03 14:13:54 +0000

    fetch data from comments to add markers on google maps

--- portfolio/src/main/webapp/mapsScript.js
+++ portfolio/src/main/webapp/mapsScript.js
@@ -1,20 +1,53 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 
 function initMap() {
     var coordin_wat = {lat: 43.4643, lng: -80.5204};
     var coordin_tor = {lat:43.6532, lng: -79.3832};
     var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 8,
+    zoom: 4,
     center: coordin_wat
     });
 
     var marker_wat = new google.maps.Marker({
     position: coordin_wat,
     map: map,
-    title: 'I\'m here'
+    title: 'Click me'
     });
     var marker_tor = new google.maps.Marker({
     position: coordin_tor,
     map: map,
-    title: 'I\'m here'
+    title: 'Click me'
     });
+    addlocations(map);
 }
+
+
+//fecth comments from the server
+function addlocations(map) {
+    fetch('/comments').then(response => response.json()).then((comments) => {
+        console.log(comments);
+        comments.forEach((comment) => {
+            var marker = new google.maps.Marker({
+                position: {lat: comment.lat, lng: comment.lng},
+                map: map,
+                title: 'Click me'
+            });
+        });
+  });
+}
+
+
+
