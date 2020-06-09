50141908be8bf7ae66f651d44df9b6a9dea93b3d
Alice Zhou <alicezhou0805@gmail.com>
2020-06-09 18:19:08 +0000

    add login/logout button to the front page

---
+++ portfolio/src/main/webapp/js/authScript.js
@@ -0,0 +1,30 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+//Get auth
+function authenticate() {
+    fetch('/auth').then(response => response.json()).then(auth => {
+        console.log(auth);
+        console.log(auth.url);
+        const logInButton = document.getElementById("authentication");
+        if (auth.Login) {
+            logInButton.innerText = "Log out";
+
+        }else{
+            logInButton.innerText = "Log in";
+        }
+        logInButton.setAttribute("href", auth.url);
+    });
+}
--- portfolio/src/main/webapp/js/keys.js
+++
@@ -1,2 +0,0 @@
-//Client ID: 362684947400-u8helelals86p2is48fmt76gtnedk1jn.apps.googleusercontent.com
-//Clinet Secret: b4iNO5WYxi-02xyi3PakWy8q
